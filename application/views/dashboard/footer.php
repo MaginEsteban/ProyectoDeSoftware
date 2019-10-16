8db2a5be51ab7a689221d0f9bf944c2b772163c3
Bruno Gorosito <gorositobruno1@gmail.com>
2019-10-16 18:36:06 -0300

    Carpeta AdminLTE en recursos para Dashboard

--- application/views/dashboard/footer.php
+++ application/views/dashboard/footer.php
@@ -15,14 +15,20 @@
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 3 -->
-<script src="<?= base_url("recursos/libs")?>/jquery/dist/jquery.js"></script>
+<script src="<?= base_url("recursos/adminlte/")?>bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="<?= base_url("recursos/libs")?>/bootstrap/dist/js/bootstrap.js"></script>
+<script src="<?= base_url("recursos/adminlte/")?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?= base_url("recursos")?>/js/adminlte.min.js"></script>
-
-<!-- <script src="<?= base_url("recursos")?>/js/dashboard/users.js"></script> -->
-
+<script src="<?= base_url("recursos/adminlte/")?>dist/js/adminlte.min.js"></script>
+<!-- jQuery 3 -->
+<!-- FastClick -->
+<script src="<?= base_url("recursos/adminlte/")?>bower_components/fastclick/lib/fastclick.js"></script>
+<!-- Sparkline -->
+<script src="<?= base_url("recursos/adminlte/")?>bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- SlimScroll -->
+<script src="<?= base_url("recursos/adminlte/")?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="<?= base_url("recursos/adminlte/")?>bower_components/chart.js/Chart.js"></script>
 
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
--- application/views/dashboard/header.php
+++ application/views/dashboard/header.php
@@ -1,78 +1,122 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title> Sistema Comedor Universitario</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> Sistema Comedor Universitario</title>
 
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('recursos')?>/css/util.css">
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url("recursos/libs")?>/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="<?= base_url("recursos/libs")?>/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url("recursos")?>/css/AdminLTE.min.css">
-  
-  <link rel="stylesheet" href="<?= base_url('recursos')?>/css/util.css">
+    <link rel="stylesheet" href="<?= base_url('recursos')?>/css/main.css">
 
-  <link rel="stylesheet" href="<?= base_url('recursos')?>/css/main.css">
-    
-  <link rel="stylesheet" href="http://weareoutman.github.io/clockpicker/dist/bootstrap-clockpicker.min.css">
+    <link rel="stylesheet"
+        href="<?= base_url('recursos/adminlte/')?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet"
+        href="<?= base_url('recursos/adminlte/')?>bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?= base_url('recursos/adminlte/')?>bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url('recursos/adminlte/')?>dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+          page. However, you can choose any other skin. Make sure you
+          apply the skin class to the body tag so the changes take effect. -->
+    <link rel="stylesheet" href="<?= base_url('recursos/adminlte/')?>dist/css/skins/skin-blue.min.css">
 
 
-  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+
+
+
+
+    <link rel="stylesheet" href="http://weareoutman.github.io/clockpicker/dist/bootstrap-clockpicker.min.css">
+
+    <!-- user menu -->
+
+
+
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect. -->
-  <link rel="stylesheet" href="<?= base_url("recursos")?>/css/skins/skin-blue.min.css">
+    <link rel="stylesheet" href="<?= base_url('recursos/adminlte/')?>dist/css/skins/skin-blue.min.css">
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 
-  <!-- Google Font -->
-  <link rel="stylesheet"
+    <!-- Google Font -->
+    <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-
-<header class="main-header">
-
-<!-- Logo -->
-<div class="row">
-<div class="col-12">
-
-<a href="<?= base_url("dashboard")?>" class="logo">
-  <!-- mini logo for sidebar mini 50x50 pixels -->
-  <span class="logo-mini"><img src="<?= base_url('recursos')?>/img/unrn.png" alt="" height="40px" style="color:white"></span>
-  <!-- logo for regular state and mobile devices -->
-  <span class="logo-lg"><b>Comedor-</b>UNRN</span>
-</a>
-
-<!-- Header Navbar -->
-<nav class="navbar navbar-static-top" role="navigation">
-  <!-- Sidebar toggle button-->
-  <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-    <span class="sr-only">Toggle navigation</span>
-  </a>
- 
-</nav>
-</div>
-</div>
-</header>
-
-
-  <!-- Contenido dinamico -->
-<div id="content" class="content-wrapper">
-
-
-
-
-   
+    <div class="wrapper">
+        <header class="main-header">
+            <!-- Logo -->
+            <a href="<?= base_url("dashboard")?>" class="logo">
+                <!-- mini logo for sidebar mini 50x50 pixels -->
+                <span class="logo-mini"><img src="<?= base_url('recursos')?>/img/unrn.png" alt="" height="40px"
+                        style="color:white"></span>
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg"><b>Comedor-</b>UNRN</span>
+            </a>
+            <!-- Header Navbar -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                </a>
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                              <i class="fa fa-user-circle-o"></i> <span>Alexander Pierce</span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header">
+                                    <img src="<?= base_url("recursos")?>/img/user2-160x160.jpg" class="img-circle"
+                                        alt="User Image">
+                                    <p>
+                                        Alexander Pierce - Web Developer
+                                        <small>Member since Nov. 2012</small>
+                                    </p>
+                                </li>
+                                <!-- Menu Body -->
+                                <li class="user-body">
+                                    <div class="row">
+                                        <div class="col-xs-4 text-center">
+                                            <a href="#">Followers</a>
+                                        </div>
+                                        <div class="col-xs-4 text-center">
+                                            <a href="#">Sales</a>
+                                        </div>
+                                        <div class="col-xs-4 text-center">
+                                            <a href="#">Friends</a>
+                                        </div>
+                                    </div>
+                                    <!-- /.row -->
+                                </li>
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- Contenido dinamico -->
+        <div id="content" class="content-wrapper">
